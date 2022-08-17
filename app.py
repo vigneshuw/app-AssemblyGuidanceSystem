9143ad465a46e6096adc01cf4e94c6283f4a2758
Vignesh <vignesh.selvaraj@fewidev.com>
2022-08-17 16:55:18 -0500

    Improvements to the UI

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout, QVBoxLayout, QStyle, QFileDialog, QLabel, QGridLayout
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout, QVBoxLayout, QStyle, QFileDialog, QLabel, \
+    QGridLayout, QComboBox, QDial, QGroupBox
 from PyQt6.QtGui import QIcon, QImage, QPixmap, QPainter
 from PyQt6.QtCharts import QBarSet, QChart, QChartView, QBarCategoryAxis, QValueAxis, QHorizontalStackedBarSeries, \
     QHorizontalPercentBarSeries
@@ -20,6 +21,11 @@ class MainWindow(QWidget):
 
         # Get the file information
         self.file_name = None
+        self.model_file_name = None
+
+        # GroupBox
+        model_gb = QGroupBox("Model parameters")
+        video_gp = QGroupBox("Video Information")
 
         # Buttons
         # Load video
@@ -36,6 +42,71 @@ class MainWindow(QWidget):
         self.cancel_btn.setEnabled(False)
         self.cancel_btn.setIcon(self.style().standardIcon(QStyle.StandardPixmap.SP_MediaStop))
         self.cancel_btn.clicked.connect(self.cancel_feed)
+        # Load the model for inference
+        self.load_model_btn = QPushButton("Load Model")
+        self.load_model_btn.setEnabled(True)
+        self.load_model_btn.clicked.connect(self.open_file_model)
+
+        # Combo boxes
+        self.assembly_selection = QComboBox()
+        self.assembly_selection.addItems(["L10", "Demo"])
+        self.assembly_selection.activated.connect(self.assembly_cb_activated)
+
+        # Dials
+        # The inference length dial
+        inference_machine_dial_vbox = QVBoxLayout()
+        self.inference_machine_dial_value_label = QLabel("30s")
+        inference_machine_dial_label = QLabel("Inference length")
+        inference_machine_dial_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.inference_machine_dial_value_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.inference_machine_dial = QDial()
+        self.inference_machine_dial.setRange(10, 60)
+        self.inference_machine_dial.setSingleStep(10)
+        self.inference_machine_dial.setValue(30)
+        self.inference_machine_dial.setEnabled(False)
+        self.inference_machine_dial.setNotchesVisible(True)
+        inference_machine_dial_vbox.addWidget(inference_machine_dial_label)
+        inference_machine_dial_vbox.addWidget(self.inference_machine_dial)
+        inference_machine_dial_vbox.addWidget(self.inference_machine_dial_value_label)
+        # Action for Dial
+        self.inference_machine_dial.valueChanged.connect(self.inference_machine_dial_value_changed)
+        # The state machine params dial
+        sm_dial_vbox1 = QVBoxLayout()
+        sm_dial_vbox2 = QVBoxLayout()
+        sm_dial1_label = QLabel("State Transition")
+        sm_dial1_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        sm_dial2_label = QLabel("Cycle Reset")
+        sm_dial2_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.sm_dial1_label_value = QLabel("3.0s")
+        self.sm_dial1_label_value.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.sm_dial2_label_value = QLabel("2.0s")
+        self.sm_dial2_label_value.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        # SM Dial-1
+        self.sm_dial1 = QDial()
+        self.sm_dial1.setRange(5, 50)
+        self.sm_dial1.setSingleStep(1)
+        self.sm_dial1.setValue(30)
+        self.sm_dial1.setEnabled(False)
+        self.sm_dial1.setNotchesVisible(True)
+        self.sm_dial1.valueChanged.connect(self.sm_dial1_value_changed)
+        sm_dial_vbox1.addWidget(sm_dial1_label)
+        sm_dial_vbox1.addWidget(self.sm_dial1)
+        sm_dial_vbox1.addWidget(self.sm_dial1_label_value)
+        # SM Dial-2
+        self.sm_dial2 = QDial()
+        self.sm_dial2.setRange(5, 50)
+        self.sm_dial2.setSingleStep(1)
+        self.sm_dial2.setValue(20)
+        self.sm_dial2.setEnabled(False)
+        self.sm_dial2.setNotchesVisible(True)
+        self.sm_dial2.valueChanged.connect(self.sm_dial2_value_changed)
+        sm_dial_vbox2.addWidget(sm_dial2_label)
+        sm_dial_vbox2.addWidget(self.sm_dial2)
+        sm_dial_vbox2.addWidget(self.sm_dial2_label_value)
+        # Add to a horizontal box
+        sm_dial_hbox = QHBoxLayout()
+        sm_dial_hbox.addLayout(sm_dial_vbox1)
+        sm_dial_hbox.addLayout(sm_dial_vbox2)
 
         # Window properties
         self.setWindowTitle("Real-time Inference")
@@ -70,6 +141,15 @@ class MainWindow(QWidget):
         hbox_btns.addWidget(self.load_video_btn)
         hbox_btns.addWidget(self.play_btn)
         hbox_btns.addWidget(self.cancel_btn)
+        video_gp.setLayout(hbox_btns)
+        # Other buttons
+        hbox_btns_2 = QHBoxLayout()
+        hbox_btns_2.setContentsMargins(0, 0, 0, 0)
+        hbox_btns_2.addWidget(self.load_model_btn)
+        hbox_btns_2.addWidget(self.assembly_selection)
+        hbox_btns_2.addLayout(inference_machine_dial_vbox)
+        hbox_btns_2.addLayout(sm_dial_hbox)
+        model_gb.setLayout(hbox_btns_2)
 
         # For the bar plots
         vbox_bar_plots = QVBoxLayout()
@@ -84,9 +164,10 @@ class MainWindow(QWidget):
         # Make a grid
         layout = QGridLayout()
         # Add Buttons
-        layout.addLayout(hbox_btns, 0, 0, 1, 10)
-        layout.addLayout(vbox_bar_plots, 1, 0, 3, 3)
-        layout.addLayout(vbox_video, 1, 3, 3, 5)
+        layout.addWidget(video_gp, 0, 0, 1, 10)
+        layout.addWidget(model_gb, 1, 0, 1, 10)
+        layout.addLayout(vbox_bar_plots, 2, 0, 3, 3)
+        layout.addLayout(vbox_video, 2, 3, 3, 5)
 
         # Initialize all the threads
         self.Worker1 = Worker1()
@@ -105,62 +186,88 @@ class MainWindow(QWidget):
         self.load_video_btn.setEnabled(True)
         self.play_btn.setEnabled(True)
 
+    def inference_machine_dial_value_changed(self):
+
+        # Set the values
+        current_value = self.inference_machine_dial.value()
+        self.Worker1.inference_length = current_value
+
+        # Update the label
+        self.inference_machine_dial_value_label.setText(str(current_value) + "s")
+
+    def sm_dial1_value_changed(self):
+
+        # Set the values
+        current_value = self.sm_dial1.value() / 10
+        self.Worker1.d1 = current_value
+
+        # Update the label
+        self.sm_dial1_label_value.setText(str(current_value) + "s")
+
+    def sm_dial2_value_changed(self):
+
+        # Set the values
+        current_value = self.sm_dial2.value() / 10
+        self.Worker1.d2 = current_value
+
+        # Update the label
+        self.sm_dial2_label_value.setText(str(current_value) + "s")
+
+    def assembly_cb_activated(self, index):
+        # Assign the values appropriately
+        if index == 0:
+            sys.stdout.write("Not Implemented\n")
+        elif index == 1:
+            # Initialize the state machines and assoc
+            self.Worker1.classes_to_states = {
+                5: 0,
+                1: 1,
+                2: 2,
+                3: 3,
+                4: 4,
+                6: 5,
+                0: 6
+
+            }
+            # Create state dependencies
+            state_dependencies = [
+                [
+                    [3, 4, 5, 6], [1],  # S0
+                ],
+
+                [
+                    [0, 6], [2]  # S1
+                ],
+
+                [
+                    [1, 6], [3]  # S2
+                ],
+
+                [
+                    [2, 6], [4]  # S3
+                ],
+
+                [
+                    [3, 6], [5]  # S4
+                ],
+
+                [
+                    [4, 6], [0]  # S5
+                ],
+
+                [
+                    [0, 1, 2, 3, 4, 5], [0, 1, 2, 3, 4, 5]  # S6 - The "Other" class
+                ],
+            ]
+            # Initialize the inference state machine
+            self.Worker1.inference_sm = StateMachine(state_dependencies=state_dependencies,
+                                                     num_classes=len(state_dependencies),
+                                                     timer=(3, 2))
+
     def start_inference(self):
         # Others
         self.load_video_btn.setEnabled(False)
         self.play_btn.setEnabled(False)
-
-        # Initialize the state machines and assoc
-        self.Worker1.classes_to_states = {
-            5: 0,
-            1: 1,
-            2: 2,
-            3: 3,
-            4: 4,
-            6: 5,
-            0: 6
-
-        }
-        # Create state dependencies
-        state_dependencies = [
-            [
-                [3, 4, 5, 6], [1],  # S0
-            ],
-
-            [
-                [0, 6], [2]  # S1
-            ],
-
-            [
-                [1, 6], [3]  # S2
-            ],
-
-            [
-                [2, 6], [4]  # S3
-            ],
-
-            [
-                [3, 6], [5]  # S4
-            ],
-
-            [
-                [4, 6], [0]  # S5
-            ],
-
-            [
-                [0, 1, 2, 3, 4, 5], [0, 1, 2, 3, 4, 5]  # S6 - The "Other" class
-            ],
-        ]
-        # Model path
-        model_path = os.path.join(os.path.dirname(os.getcwd()), "trained_models", "AssemblyDemo", "c3d",
-                                  "C3D_scratch_best_optimo.hdf5")
-        model = keras.models.load_model(model_path)
-
-        # Real time class
-        self.Worker1.c3d_realtime = C3DOpticalFlowRealTime(model, inference_length=30)
-        # Initialize the inference state machine
-        self.Worker1.inference_sm = StateMachine(state_dependencies=state_dependencies, num_classes=len(state_dependencies),
-                                    timer=(3, 2))
 
         # Start the worker operation
         self.Worker1.start()
@@ -173,8 +280,33 @@ class MainWindow(QWidget):
         if file_name != '':
             self.file_name = file_name
             self.Worker1.file_name = file_name
-            self.play_btn.setEnabled(True)
-            self.cancel_btn.setEnabled(True)
+            if self.model_file_name is not None:
+                # self.play_btn.setEnabled(True)
+                # self.cancel_btn.setEnabled(True)
+                # Enable all dials
+                self.inference_machine_dial.setEnabled(True)
+                self.sm_dial1.setEnabled(True)
+                self.sm_dial2.setEnabled(True)
+
+    def open_file_model(self):
+
+        file_name, _ = QFileDialog.getOpenFileName(self, "Open Model")
+
+        # Set model to model file name
+        if file_name != "":
+            self.model_file_name = file_name
+            self.Worker1.model_file_name = file_name
+            if self.file_name is not None:
+                # self.play_btn.setEnabled(True)
+                # self.cancel_btn.setEnabled(True)
+                # Enable all dials
+                self.inference_machine_dial.setEnabled(True)
+                self.sm_dial1.setEnabled(True)
+                self.sm_dial2.setEnabled(True)
+
+        # Load and set the required model
+        model = keras.models.load_model(self.model_file_name)
+        self.Worker1.c3d_realtime = C3DOpticalFlowRealTime(model=model, inference_length=30)
 
     def initialize_charts(self, num_steps):
 
@@ -236,11 +368,18 @@ class Worker1(QThread):
         self.file_name = None
         self.thread_active = False
 
+        # Model information
+        self.model_file_name = None
+
         # The Inference module
+        # Hyperparams
+        self.inference_length = None
+        self.d1 = None
+        self.d2 = None
+
         self.c3d_realtime = None
         self.inference_sm = None
         self.classes_to_states = None
-
 
     def run(self):
 
